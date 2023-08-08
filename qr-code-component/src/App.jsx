59644d21e589105468b1b99b7042275c804316cd
Huy Hoang Le <hhoangle2323@gmail.com>
2023-08-08 18:23:24 +0700

    upload qr-code component

---
+++ qr-code-component/src/App.jsx
@@ -0,0 +1,29 @@
+import './app.css'
+import imgUrl from './assets/images/image-qr-code.png'
+
+export function App() {
+  return (
+    <div class="container">
+      <div className="img">
+        <img src={imgUrl} alt="alt" />
+      </div>
+      <div className="description">
+        <h2 className="header">
+          Improve your front-end skills by building projects
+        </h2>
+        <div className="detail">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </div>
+        <div class="attribution">
+          Challenge by{' R1o the dev '}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">R1o</a>.
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
